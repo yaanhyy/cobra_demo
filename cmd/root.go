b6a5e1e8f97d0a66c824c32309bded0806617327
洪远洋 <hongyuanyang@huobi.com>
2019-03-08 19:05:04 +0800

    add cmd version

--- cmd/root.go
+++ cmd/root.go
@@ -57,7 +57,7 @@ var rootCmd = &cobra.Command{
                 love by spf13 and friends in Go.
                 Complete documentation is available at http://hugo.spf13.com`,
 	Run: func(cmd *cobra.Command, args []string) {
-		print("hello")
+		fmt.Println("hello")
 		// Do Stuff Here
 	},
 }
